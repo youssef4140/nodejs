58586a6f636f05b8af82bb1ef125e4da787445cc
youssef4140 <88176339+youssef4140@users.noreply.github.com>
2023-06-20 13:44:25 +0300

    Add files via upload

---
+++ task4express/model/products.js
@@ -0,0 +1,15 @@
+export let products;
+export const get = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+        products = data
+
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+
+  
+  
